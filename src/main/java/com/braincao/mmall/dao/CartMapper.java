a35858eeaec71dbe2af6e169fe702a44d31ba4aa
braincao <cby491316377@qq.com>
2019-03-26 00:57:26 +0800

    build ssm framework -done

---
+++ src/main/java/com/braincao/mmall/dao/CartMapper.java
@@ -0,0 +1,18 @@
+package com.braincao.mmall.dao;
+
+import com.braincao.mmall.pojo.Cart;
+
+public interface CartMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(Cart record);
+
+    int insertSelective(Cart record);
+
+    Cart selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(Cart record);
+
+    int updateByPrimaryKey(Cart record);
+
+}
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -1,5 +1,9 @@
 <html>
 <body>
+<<<<<<< HEAD
 <h2>Hello World git test!</h2>
+=======
+<h2>Hello World!</h2>
+>>>>>>> build ssm framework -done
 </body>
 </html>
